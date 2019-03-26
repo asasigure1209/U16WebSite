ceffb0dabb68b07928f4d0823d4f8a273d82a269
asasigure <mkmokymh@gmail.com>
2019-03-26 23:56:49 +0900

    add: pug template

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,12 @@ const sass = require('gulp-sass')
 const autoprefixer = require('gulp-autoprefixer')
 const uglify = require('gulp-uglify')
 const browser = require('browser-sync')
+const pug = require('gulp-pug')
 
 const SASS_PATH = 'src/sass/**/*.sass'
 const JS_PATH = 'src/js/**/*.js'
-const SASS_OUTPUT_PATH = './dist/css'
+const PUG_PATH = 'src/pug/**/*.pug'
+const CSS_OUTPUT_PATH = './dist/css'
 const JS_OUTPUT_PATH = './dist/js'
 const HTML_OUTPUT_PATH = './dist'
 
@@ -22,7 +24,7 @@ task('sass', () => {
     return src(SASS_PATH)
         .pipe(sass())
         .pipe(autoprefixer())
-        .pipe(dest(SASS_OUTPUT_PATH))
+        .pipe(dest(CSS_OUTPUT_PATH))
         .pipe(browser.reload({stream: true}))
 })
 
@@ -33,9 +35,17 @@ task('js', () => {
         .pipe(browser.reload({stream: true}))
 })
 
+task('pug', () => {
+    return src(PUG_PATH)
+        .pipe(pug())
+        .pipe(dest(HTML_OUTPUT_PATH))
+        .pipe(browser.reload({stream: true}))
+})
+
 task('watch', () => {
     watch(SASS_PATH, task('sass'))
     watch(JS_PATH, task('js'))
+    watch(PUG_PATH, task('pug'))
 }) 
 
 task('default', parallel(
